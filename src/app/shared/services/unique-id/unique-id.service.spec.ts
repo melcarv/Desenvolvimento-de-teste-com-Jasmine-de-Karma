e0fe0f02ff10fa6b236e9112f0a554216cf80887
melcarv <melm.dev@gmail.com>
2022-03-17 16:17:14 -0300

    construção do código a ser testado e teste

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,19 @@
+import { UniqueIdService } from './unique-id.service';
+
+describe(UniqueIdService.name, () => {
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+        const service = new UniqueIdService();
+        const id = service.generateUniqueIdWithPrefix('app');
+        expect(id.startsWith('app-')).toBeTrue();
+    });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicated ids when called multiple times`, () => {
+        const service = new UniqueIdService();
+        const ids = new Set();
+        for (let i = 0; i <50 ; i++) {
+            ids.add(service.generateUniqueIdWithPrefix('app'));
+        }
+
+        expect(ids.size).toBe(50);
+    })
+});
