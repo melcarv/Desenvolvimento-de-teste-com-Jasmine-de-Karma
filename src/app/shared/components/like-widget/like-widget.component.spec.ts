8b85b38dd372e3a4d983331af8aaf4cbaecace2f
melcarv <melm.dev@gmail.com>
2022-03-30 21:16:34 -0300

    Formulação do teste

---
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { LikeWidgetComponent } from './like-widget.component';
+
+describe(LikeWidgetComponent.name, () => {
+    let fixture: ComponentFixture<LikeWidgetComponent> = null;
+
+    beforeEach(async() => {
+        await TestBed.configureTestingModule({
+            declarations: [LikeWidgetComponent],
+            providers: [UniqueIdService],
+            imports: [FontAwesomeModule]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(LikeWidgetComponent);
+    });
+
+    it('Should create component', ()=> {
+        const instance = fixture.componentInstance;
+        expect(instance).toBeTruthy();
+    });
+});
